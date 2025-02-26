76ac026083a119704ff663df803a9f49ae576a91
User Ahmed <uchihamadara20488@gmail.com>
2025-02-26 17:45:03 +0300

    Спринт 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,83 +2,68 @@ package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.services.UserService;
+import ru.yandex.practicum.filmorate.storages.InMemoryUserStorage;
 
-import java.time.LocalDate;
 import java.util.*;
 
 @RestController
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
-    private Map<Long, User> users = new HashMap<>();
+    private final InMemoryUserStorage inMemoryUserStorage;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(InMemoryUserStorage inMemoryUserStorage,
+                          UserService userService) {
+        this.inMemoryUserStorage = inMemoryUserStorage;
+        this.userService = userService;
+    }
+
 
     @GetMapping
     public Collection<User> getAllUsers() {
-        return this.users.values();
+        return inMemoryUserStorage.getAllUsers();
+    }
+
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public List<User> addFriend(@PathVariable("id") int id,
+                                @PathVariable("friendId") int friendId) {
+        return userService.addFriend(id, friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriend(@PathVariable("id") int id,
+                             @PathVariable("friendId") int friendId) {
+        userService.deleteFriend(id, friendId);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> listFriendWithOtherUser(@PathVariable("id") int id,
+                                              @PathVariable("otherId") int otherId) {
+        return userService.getListFriendsWithOtherUser(id, otherId);
+    }
+
+    @GetMapping("/{id}/friends")
+    public List<User> getFriendsUser(@PathVariable("id") int id) {
+        return userService.getFriends(id);
     }
 
     @PostMapping
     public User addUser(@Valid @RequestBody User user) {
-        if (user.getEmail() == null) {
-            log.info("Ошибка при добавлении пользователя. Электронный адрес не может быть пустой");
-            throw new ValidationException("Электронный адрес не может быть пустой");
-        }
-        if (!user.getEmail().contains("@")) {
-            log.info("Ошибка при добавлении пользователя. Электронный адрес должен содержать @");
-            throw new ValidationException("Электронный адрес должен содержать @");
-        }
-        if (user.getLogin() == null) {
-            log.info("Ошибка при добавлении пользователя. Логин не может быть пустым");
-            throw new ValidationException("Логин не может быть пустым");
-        }
-        if (user.getLogin().contains(" ")) {
-            log.info("Ошибка при добавлении пользователя. Логин не может содержать пробелы");
-            throw new ValidationException("Логин не может содержать пробелы");
-        }
-        if (user.getName() == null) {
-            user.setName(user.getLogin());
-        }
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            log.info("Ошибка при добавлении пользователя. Неверно введена дата рождения");
-            throw new ValidationException("Неверно введена дата рождения");
-        }
-        user.setId(this.getNextId());
-        this.users.put(user.getId(), user);
+        inMemoryUserStorage.addUser(user);
         return user;
     }
 
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
-        if (user.getId() == null) {
-            log.info("Ошибка при добавлении пользователя. Не введен id пользователя");
-            throw new ValidationException("Не введен id пользователя");
-        }
-        if (user.getEmail() == null) {
-            log.info("Ошибка при добавлении пользователя. Email не может быть пустым");
-            throw new ValidationException("Email не может быть пустым");
-        }
-        if (user.getLogin() == null) {
-            log.info("Ошибка при добавлении пользователя. Логин не может быть пустым");
-            throw new ValidationException("Логин не может быть пустым");
-        }
-        User updateUser = users.get(user.getId());
-        updateUser.setLogin(user.getLogin());
-        updateUser.setEmail(user.getEmail());
-        updateUser.setName(user.getName());
-        updateUser.setBirthday(user.getBirthday());
-        return updateUser;
+        return inMemoryUserStorage.updateUser(user);
     }
 
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
-    }
 }
